9f686de6eb7ed3439292465af079f177aba49cdb
Dan Apone <dapone@gmail.com>
2015-07-13 14:23:50 -0700

    using express to serve static files

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-
-var app = connect();
-
-app.use(serveStatic(__dirname, {'index': ['index.html']}));
-app.listen(3000);
+var express = require('express');
+ 
+var server = express();
+server.use(express.static(__dirname + '/public'));
+ 
+var port = 10001;
+server.listen(port, function() {
+    console.log('server listening on port ' + port);
+});
