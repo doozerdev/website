eb5ffadc7d7c11b69b189681125cf5854f0059fe
Dan Apone <dapone@gmail.com>
2015-07-13 14:08:43 -0700

    added packages

--- server.js
+++ server.js
@@ -1,12 +1,7 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('Hello Node.js!\n');
-    if ('host' in req.headers) {
-        var host = req.headers['host'];
-        res.write('Vhost is ' + host.split(':')[0] + '\n');
-    } else {
-        res.write('No vhost specified\n');
-    }
-    res.end();
-}).listen(process.env.PORT);
+var connect = require('connect');
+var serveStatic = require('serve-static');
+
+var app = connect();
+
+app.use(serveStatic(__dirname, {'index': ['index.html']}));
+app.listen(3000);
