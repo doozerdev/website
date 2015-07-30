4b206de281a48b4dded21639cee23e17521db2e9
rdeutsch <rdeutsch@gmail.com>
2015-07-29 18:31:01 -0700

    google analytics

--- public/js/bskit-scripts.js
+++ public/js/bskit-scripts.js
@@ -2,14 +2,21 @@
 /*	Javascript code for all elements
 /*----------------------------------------------------*/
 
+ 
 
 /* -------- Owl Carousel -------- */
-$(".scenario-carousel").owlCarousel({
-	slideSpeed : 300,
-	paginationSpeed : 400,
-	singleItem : true,
-    autoPlay : 4000,		
+$(document).ready(function() {
+	$(".scenario-carousel").owlCarousel({
+		slideSpeed : 300,
+		paginationSpeed : 600,
+		rewindSpeed: 2000,
+		singleItem : true,
+	    autoPlay : 8000,
+		stopOnHover: true,		
+	});
+	console.log("done config carousel");
 });
+
 // End Owl Carousel
 
 
